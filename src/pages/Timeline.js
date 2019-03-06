51bda861c3767e126beaf80bf97659460c7904f1
Murilo Maldonado Mendes <murilomaldonadom@gmail.com>
2019-03-06 15:21:11 -0300

    feat: List all tweets

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -4,9 +4,17 @@ import api from '../services/api'
 import twitterLogo from '../twitter.svg';
 import './Timeline.css';
 
+import Tweet from '../components/Tweet';
+
 export default class Timeline extends Component {
 	state = {
+		tweets: [],
 		newTweet: ''
+	}
+
+	async componentDidMount() {
+		const response = await api.get('tweets');
+		this.setState({ tweets: response.data });
 	}
 
 	handleInputChange = e => {
@@ -14,24 +22,30 @@ export default class Timeline extends Component {
 	}
 
 	handleNewTweet = async e => {
-		if(e.keyCode !== 13) return;
+		if (e.keyCode !== 13) return;
 
 		const content = this.state.newTweet;
 		const author = localStorage.getItem('@GoTwitter:username');
-		
+
 		await api.post('tweets', { content, author })
 
 		this.setState({ newTweet: '' });
 	}
 
 	render() {
-    	return (
+		return (
 			<div className="timeline-wrapper">
 				<img height={24} src={twitterLogo} alt="GoTwitter"></img>
 				<form>
 					<textarea value={this.state.newTweet} onChange={this.handleInputChange} onKeyDown={this.handleNewTweet} placeholder="O que está acontecendo?" />
 				</form>
+
+				<ul className="tweet-list">
+					{this.state.tweets.map(tweet => (
+						<Tweet key={tweet._id} tweet={tweet} />
+					))}
+				</ul>
 			</div>
 		);
-  	}
+	}
 }
